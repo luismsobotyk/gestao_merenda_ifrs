a58639940e167b97df881a76160ae5948b9775ee
luismsobotyk <luismsobotyk@gmail.com>
2026-06-25 11:57:08 -0300

    cosmetic: alinhamento horario e turno

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -430,7 +430,7 @@
                     <section class="menu">
                         @foreach($dia['horarios'] as $hIndex => $horario)
                             <article class="period p-{{ $hIndex % 4 }}">
-                                <div class="d-flex align-items-center justify-content-between w-100 mb-3">
+                                <div class="d-flex align-items-center justify-content-between justify-content-sm-center gap-sm-4 w-100 mb-4">
                                     <div class="d-flex align-items-center gap-3">
                                         <span class="period-ico m-0">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" viewBox="0 0 16 16"><path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/><path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/></svg>
